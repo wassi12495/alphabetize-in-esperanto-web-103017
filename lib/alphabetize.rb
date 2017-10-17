6a6bcd26670b05c6f5424fdbcac4b5941940b462
github username <github email address>
2017-10-17 18:03:46 -0400

    Finished Alphabetize in esperanto

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+require "pry"
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
+
   # code here
+  arr.sort_by do |word|
+    word.split("").collect do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
+
+
+    #binding.pry
+
 end
